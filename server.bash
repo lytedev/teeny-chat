bfc84bd97f5b9af805840c0a9d2b9c4d044c88a5
Daniel Flanagan <daniel@lytedev.io>
2018-12-04 08:39:05 -0600

    Prevent simple empty strings and proper auto-scrolling

--- server.bash
+++ server.bash
@@ -4,10 +4,17 @@ log_file="./chat.log"
 touch "$log_file"
 m() { echo "[$(date +%H:%M:%S)] $*" >> "$log_file"; }
 echo "You can start typing anytime. Text is entered in the bottom-left. "
-echo -e "The send button is located in the bottom-right. You may also hit the enter key to send.\n"
-echo "Please enter your username:"; read USER
+echo -e "The send button is located in the bottom-right. You may also hit the enter key to send.\\n"
+USER=""
+while [ -z "$USER" ]; do
+	echo "Please enter your username (not blank):"; read -r USER
+done
 echo ""
 m "${USER} joined."
-echo -e "Welcome to the chat ${USER}!\n"
+echo -e "Welcome to the chat ${USER}!\\n"
 tail -n 0 -f "$log_file" --pid=$$ &
-while read MSG; do m "${USER}: ${MSG}"; done
+while read MSG; do
+	if [ -z "$MSG" ]; do
+		m "${USER}: ${MSG}";
+	fi
+done
